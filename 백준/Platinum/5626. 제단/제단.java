55ab0a5af204524bdbec13564a59aad469d79265
Donggeon Lee <83354877+dglee2007@users.noreply.github.com>
2024-08-09 10:15:08 +0900

    [Platinum V] Title: 제단, Time: 472 ms, Memory: 24028 KB -BaekjoonHub

--- 백준/Platinum/5626. 제단/제단.java
+++ 백준/Platinum/5626. 제단/제단.java
@@ -1,40 +1,46 @@
 import java.io.*;
 import java.util.*;
-public class Main {
-    static final int MOD = 1000000007;
-    static int N, H;
-    static int d[][];
 
+public class Main {
+    static int N, H;
+    static final int MOD = 1000000007;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine().trim());
-        H = N/2;
+        H=N/2;
+        int[] arr;
+        arr = new int[N+1];
+        dp = new int[2][H+3];
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        for (int i = 1; i <= N; i++) {arr[i]=Integer.parseInt(st.nextToken());}
+        //제단의 높이가 -1이면 1, 제단의 높이가 0이면 1, arr[0]>0이면 0
+        dp[0][0] = arr[1]==-1?1:arr[1]==0?1:0;
+        //dp[0][0] = (arr[1] == -1 || arr[1] == 0) ? 1 : 0;
 
-        int a[] = new int[N+1];
-
-        d = new int[N+1][H+3];
-
-        StringTokenizer st = new StringTokenizer(br.readLine().trim()); 
-        for ( int i=1; i<=N; i++ ) a[i] = Integer.parseInt(st.nextToken());
-
-        d[1][0] = a[1]==-1?1:a[1]==0?1:0;
-        for ( int i=2; i<=N; i++ ) {
-            if ( a[i] == -1 ) {
-                d[i][0] = (d[i-1][0] + d[i-1][1])%MOD;
-                for ( int j=1; j<=H; j++ ) {
-                    d[i][j] = ((d[i-1][j-1] + d[i-1][j])%MOD + d[i-1][j+1])%MOD;
+        for (int i=2; i <= N; i++) {
+            if (arr[i]==-1){
+                dp[1][0] = (dp[0][0]+dp[0][1])%MOD;
+                for (int j = 1; j <= H; j++) {
+                    dp[1][j]=((dp[0][j-1]+dp[0][j])%MOD+dp[0][j+1])%MOD;
                 }
-            } else if ( a[i] == 0 ) {
-                d[i][0] = (d[i-1][0] + d[i-1][1])%MOD;
-            } else {
-                d[i][a[i]] = ((d[i-1][a[i]-1] + d[i-1][a[i]])%MOD + d[i-1][a[i]+1])%MOD;
+            }
+            else if (arr[i]==0){
+                dp[1][0] = (dp[0][0] + dp[0][1])%MOD;
+            }
+            else {
+                dp[1][arr[i]]=((dp[0][arr[i]-1]+dp[0][arr[i]])%MOD+dp[0][arr[i]+1])%MOD;
+            }
+
+            for (int j=0;j<=H;j++) {
+                dp[0][j]=dp[1][j];
+                dp[1][j]=0;
             }
         }
-
-        bw.write(d[N][0]+"\n");
+        bw.write(dp[0][0]+"\n");
         bw.flush();
         bw.close();
-        br.close();    
-   }
+        br.close();
+    }
 }
