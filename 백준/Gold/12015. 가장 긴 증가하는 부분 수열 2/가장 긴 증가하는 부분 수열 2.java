b5c982ff20de80f47ffc7c313b35f0f82c87cad0
Donggeon Lee <83354877+dglee2007@users.noreply.github.com>
2024-08-23 18:04:28 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 620 ms, Memory: 122328 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,90 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    final static int MAX_N      =   1000000;
-    static int N, OFFSET;
-    static int[] tree;
-    static Num[] nums;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-        nums = new Num[N];
-
-        for (OFFSET = 1; OFFSET < N; OFFSET *= 2);
-        tree = new int[OFFSET * 2];
-
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < N; i++) {
-            nums[i] = new Num(Integer.parseInt(st.nextToken()), i);
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(nums);
+        List<Integer> lis = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
-            int l = query(0, nums[i].i);
-            update(nums[i].i, ++l);
-        }
-
-        bw.write(tree[1] + "\n");
-
-        bw.flush();
-    }
-
-    static int merge(int l, int r) {
-        return Math.max(l, r);
-    }
-
-    static void update(int i, int v) {
-        i += OFFSET;
-        tree[i] = v;
-        while ((i /= 2) > 0) {
-            tree[i] = merge(tree[i * 2], tree[i * 2 + 1]);
-        }
-    }
-
-    static int query(int s, int e) {
-        int res = 0;
-
-        s += OFFSET;
-        e += OFFSET;
-        while (s <= e) {
-            if (s % 2 == 1) {
-                res = merge(res, tree[s]);
-                s++;
+            int num = A[i];
+            if (lis.isEmpty() || lis.get(lis.size() - 1) < num) {
+                lis.add(num);
+            } else {
+                int pos = Collections.binarySearch(lis, num);
+                if (pos < 0) pos = -(pos + 1);
+                lis.set(pos, num);
             }
-            if (s % 2 == 0) {
-                res = merge(res, tree[e]);
-                e--;
-            }
-            s /= 2;
-            e /= 2;
         }
 
-        return res;
+        System.out.println(lis.size());
     }
-
-    static class Num implements Comparable<Num> {
-        int n, i;
-
-        public Num(int n, int i) {
-            this.n = n;
-            this.i = i;
-        }
-
-        @Override
-        public int compareTo(Num o) {
-            if (this.n == o.n) return Integer.compare(o.i, this.i);
-            else return Integer.compare(this.n, o.n);
-        }
-
-    }
-
 }
